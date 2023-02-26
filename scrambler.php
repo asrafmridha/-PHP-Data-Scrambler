fe15f9689c4a0d23a38929cdf893b13e508ee61d
asraf-mridha <asrafmridha@gmail.com>
2023-02-26 20:16:36 +0600

    Design by Bootstarp

--- scrambler.php
+++ scrambler.php
@@ -9,15 +9,40 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Data Scrambler</title>
 </head>
-
 <body>
     <div class="row offset-3 mt-5">
-        
+        <h1>Data Scrambler</h1>
+        <p class="mt-2">Use This Application Scramble Your data</p>
+        <div class="col-md-8">
+            <a href="" class="btn btn-success">Encode</a>
+            <a href="" class="btn btn-success">Decode</a>
+            <a href="" class="btn btn-success">Generate Key</a>
+        </div>
+        <div class="col-md-8 mt-3">
+            <label for="">Key</label>
+            <input class="form-control" type="text">
+        </div>
+        <div class="col-md-8 mt-3">
+            <label for="">Data</label>
+            <div class="form-floating">
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 150px"></textarea>
+                <label for="floatingTextarea2"></label>
+            </div>
+        </div>
+        <div class="col-md-8 mt-2">
+            <label for="">Result</label>
+            <div class="form-floating">
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 150px"></textarea>
+                <label for="floatingTextarea2"></label>
+            </div>
+        </div>
 
+        <div class="col-md-8 mt-2">
+            <button type="button" class="btn btn-primary">Do It For Me</button>
+        </div>
     </div>
-
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
