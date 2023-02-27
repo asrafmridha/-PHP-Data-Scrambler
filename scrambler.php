de11d7b5243ae054218ef0e4c987e2e288fe05da
asraf-mridha <asrafmridha@gmail.com>
2023-02-27 21:50:40 +0600

    form Submit

--- scrambler.php
+++ scrambler.php
@@ -2,9 +2,11 @@
 include_once "scramblerfunction.php";
 $task = "encode";
 if (isset($_GET['task']) && $_GET['task'] != '') {
+
     $task = $_GET['task'];
 }
 $key  = 'abcdefghijklmnopqrstuvwxyz1234567890';
+
 if ('key' == $task) {
     $key_original = str_split($key);
     shuffle($key_original);
@@ -35,7 +37,7 @@ if ('key' == $task) {
             <a href="/scramblerfunction.php?task=decode" class="btn btn-success">Decode</a>
             <a href="/ostad_php_project/DataScramblerProject/scrambler.php?task=key" class="btn btn-success">Generate Key</a>
         </div>
-        <form action="" method="POST">
+        <form action="/ostad_php_project/DataScramblerProject/scrambler.php" method="POST">
             <div class="col-md-8 mt-3">
                 <label for="">Key</label>
                 <input class="form-control" type="text" name="key" id="key" <?php displayKey($key) ?>>
@@ -56,10 +58,11 @@ if ('key' == $task) {
             </div>
 
             <div class="col-md-8 mt-2">
-                <button type="button" class="btn btn-primary">Do It For Me</button>
+                <button type="submit" class="btn btn-primary">Do It For Me</button>
             </div>
+        </form>
     </div>
-    </form>
+
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
